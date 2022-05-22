9aac4ae14c2b3e1360321c9fba580ac35a87c6bf
milesdavis71 <hevesiistvan71@hotmail.com>
2022-05-22 11:05:36 +0200

    03/053 Adding Content to the Recipes Components - kész

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model.js';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'This is simple a text',
+      'https://www.archanaskitchen.com/images/archanaskitchen/Indian_Vegetables_Dry/Quick_Cauliflower_Sabzi_Recipe-1.jpg'
+    ),
+  ];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
