e3662c577b4f2139e597fcf8bda44f513e6c16e7
Hevesi Istvan <hevesiistvan71@hotmail.com>
2022-06-28 15:37:33 +0200

    Második  teszt recept létrehozása a recipe-list.component.ts-ben.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -13,6 +13,11 @@ export class RecipeListComponent implements OnInit {
       'This is simple a text',
       'https://www.archanaskitchen.com/images/archanaskitchen/Indian_Vegetables_Dry/Quick_Cauliflower_Sabzi_Recipe-1.jpg'
     ),
+    new Recipe(
+      'An another test recipe',
+      'This is simple a text',
+      'https://www.archanaskitchen.com/images/archanaskitchen/Indian_Vegetables_Dry/Quick_Cauliflower_Sabzi_Recipe-1.jpg'
+    ),
   ];
   constructor() {}
 
