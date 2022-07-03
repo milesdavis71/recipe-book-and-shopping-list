5308f79e6e9637f1fd43c53e4e21fe6556cc5c82
Hevesi Istvan <hevesiistvan71@hotmail.com>
2022-07-03 09:40:00 +0200

    089 Allowing the User to Add Ingredients to the Shopping List kész. app.module.ts-hez hozzáadva a FormsModule.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -15,4 +15,8 @@ export class ShoppingListComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  onIngredientAdded(newIngredient: Ingredient) {
+    this.ingredients.push(newIngredient);
+  }
 }
